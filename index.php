c76dbb23e6b211784a6793d7ca706d6b8cde64dd
Santo77777 <55029425+Santo77777@users.noreply.github.com>
2020-02-28 19:26:30 -0500

    Se agrega array indexado y asociativo

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$clase1 = "Clase 4 bucles, arreglos y funciones";
+echo $clase1;
+
+//Arreglos indexado 
+
+$arreglo= array("Santiago", "Andres", "Maria", "Emilio", "Carolina");
+echo "<br>";
+print_r($arreglo);
+echo "<br>*********************************************************************";
+echo "<br>";
+
+//Arreglos asociativos 
+
+$arreglo = array('usuario1'=>"Santiago", 'usuario2'=> "Andres", 'usuario3'=>"Maria", 'usuario4'=>"Emilio");
+echo "<br>";
+print_r($arreglo);
+echo "<br>*********************************************************************";
+echo "<br>";
+
+
+
+
+
+?>
