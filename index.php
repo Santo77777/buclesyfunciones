f90c5c50e3ec2753bb2beb78c024de72388a4da2
Santo77777 <55029425+Santo77777@users.noreply.github.com>
2020-02-28 20:55:25 -0500

    Bucles FOREACH en ejemplos indexados y asociativos

--- index.php
+++ index.php
@@ -19,6 +19,60 @@ echo "<br>*********************************************************************"
 echo "<br>";
 
 
+// Tamaño de arreglo
+
+$size= count($arreglo);
+echo "<br>";
+echo ("El tamaño de nuetro arrelgo es: ". $size);
+echo "<br>*********************************************************************";
+echo "<br>";
+
+//Bucle FOR
+
+for($centinela=0; $centinela<=10; $centinela++){
+
+    echo("El valor de centinela es: " . $centinela . "<br>");
+}
+
+echo "<br>*********************************************************************";
+echo "<br>";
+
+//Bucle FOR para recorrer arreglos
+$arreglo= array("Santiago", "Andres", "Maria", "Emilio", "Carolina");
+
+
+for($x =0; $x < count($arreglo); $x++){
+    echo("<br>" . $arreglo[$x] . "<br>");
+
+}
+echo "<br>*********************************************************************";
+echo "<br>";
+
+//Bucle FOREACH
+
+$arreglo= array('usuario1'=>"Santiago", 'usuario2'=> "Andres", 'usuario3'=>"Maria", 'usuario4'=>"Emilio");
+
+foreach($arreglo as $clave=>$valor){
+    echo "La clave es: " . $clave;
+    echo "<br>" . ($valor) . "<br>"; 
+    echo "<br>";
+}
+echo "<br>*********************************************************************";
+echo "<br>";
+
+//Bucle foreach para arreglo indexado  en valor
+
+echo "En VALOR";
+$arreglo= array("Santiago", "Andres", "Maria", "Emilio", "Carolina");
+foreach($arreglo as $clave=>$valor){
+    echo "<br> La CLAVE del cajon es: " . $clave;
+    echo "<br> El VALOR del cajon es: " . $valor;
+    
+}
+echo "<br>*********************************************************************";
+echo "<br>";
+
+
 
 
 
